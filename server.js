3b9a2e62f726168bf1cebeb355318da909bea84c
Gabriel da Silva Rocha <115048603+bielkh@users.noreply.github.com>
2022-12-19 08:49:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; 
 
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/:resource/list': '/:resource',
-    '/:resource/get/:id': '/:resource/:id',
-    '/:resource/create': '/:resource',
-    '/:resource/update/:id': '/:resource/:id',
-    '/:resource/delete/:id': '/:resource/:id',
-  }),
-);
 server.use(router);
 
 server.listen(port);
